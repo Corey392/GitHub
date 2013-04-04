6b216d3643a605d64d356affd0d1feca355ba1e6
Todd Wiggins <Todd@Todd-Laptop-Dev>
2013-04-04 15:32:58 +1100

    File Structure clean up, moved 'RPLScripts.js' to the 'scripts' folder

--- src/java/util/RPLFragment.java
+++ src/java/util/RPLFragment.java
@@ -12,16 +12,16 @@ public enum RPLFragment {
     HEADER("/WEB-INF/jspf/header.jspf"),
     FOOTER("/WEB-INF/jspf/footer.jspf"),
     MAINTENANCE_OPTIONS("/WEB-INF/jspf/maintenanceOptions.jspf"),
-    JAVASCRIPT("/RPLScripts.js");
-    
+    JAVASCRIPT("/scripts/RPLScripts.js");
+
     public final String relativeAddress;
     public final String absoluteAddress;
-    
+
     RPLFragment(String relativeAddress) {
         this.relativeAddress = relativeAddress;
         this.absoluteAddress = RPLPage.ROOT + relativeAddress;
     }
-    
+
     @Override
     public String toString() {
         return this.absoluteAddress;
