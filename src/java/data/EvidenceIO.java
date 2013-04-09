5d3e1afa660f4dba2857b096e64b1b608e3b1b76
koroshiya <korocresent@gmail.com>
2013-04-09 17:45:43 +1000

    Update EvidenceIO.java
    Updated to reflect changes made to Evidence.java + general code cleanup

--- src/java/data/EvidenceIO.java
+++ src/java/data/EvidenceIO.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
 import domain.Evidence;
@@ -21,7 +17,6 @@ import util.Util;
 public class EvidenceIO extends RPL_IO<Evidence> {
 
     public enum Field {
-        STUDENT_ID("studentID"),
         CLAIM_ID("claimID"),
         ELEMENT_ID("elementID"),
         DESCRIPTION("description"),
@@ -41,95 +36,89 @@ public class EvidenceIO extends RPL_IO<Evidence> {
     }
 
     public void insert(Evidence evidence) throws SQLException {
+        
         String sql = "SELECT fn_InsertEvidence(?,?,?,?,?)";
         
-        String studentID = evidence.getStudentID();
         int claimID = evidence.getClaimID();
         Integer elementID = evidence.getElementID();
         String description = evidence.getDescription();
         String moduleID = evidence.getModuleID();
         
-        SQLParameter p1, p2, p3, p4, p5;
-        p1 = new SQLParameter(studentID);
-        p2 = new SQLParameter(claimID);
-        p3 = new SQLParameter(elementID);
-        p4 = new SQLParameter(description);
-        p5 = new SQLParameter(moduleID);
+        SQLParameter p1 = new SQLParameter(claimID);
+        SQLParameter p2 = new SQLParameter(elementID);
+        SQLParameter p3 = new SQLParameter(description);
+        SQLParameter p4 = new SQLParameter(moduleID);
         
-        super.doPreparedStatement(sql, p1, p2, p3, p4, p5);
+        super.doPreparedStatement(sql, p1, p2, p3, p4);
     }
     
     /**
-     * Updates evidence. Calls one of two database funcitons depending
+     * Updates evidence. Calls one of two database functions depending
      * on whether the claim type is RPL or PREVIOUS_STUDIES. Claim
      * type is determined by evidence.elementID.
      * @param evidence
      * @throws SQLException 
      */
     public void update(Evidence evidence) throws SQLException {
+        
         String sql;
-
-        String studentID, moduleID;
-        int claimID, elementID;
-        String description, assessorNote;
-        boolean approved;        
         
-        studentID = evidence.getStudentID();
-        claimID = evidence.getClaimID();
-        moduleID = evidence.getModuleID();
-        description = evidence.getDescription();
-        approved = evidence.isApproved();
-        assessorNote = evidence.getAssessorNote();
-        elementID = evidence.getElementID();
+        int claimID = evidence.getClaimID();
+        String moduleID = evidence.getModuleID();
+        String description = evidence.getDescription();
+        boolean approved = evidence.isApproved();
+        String assessorNote = evidence.getAssessorNote();
+        int elementID = evidence.getElementID();
         
-        SQLParameter p1, p2, p3, p4, p5, p6, p7;
-        p1 = new SQLParameter(studentID);
-        p2 = new SQLParameter(claimID);
-        p3 = new SQLParameter(moduleID);
-        p4 = new SQLParameter(description);
-        p5 = new SQLParameter(approved);
-        p6 = new SQLParameter(assessorNote);
-        p7 = new SQLParameter(elementID);
+        SQLParameter p1 = new SQLParameter(claimID);
+        SQLParameter p2 = new SQLParameter(moduleID);
+        SQLParameter p3 = new SQLParameter(description);
+        SQLParameter p4 = new SQLParameter(approved);
+        SQLParameter p5 = new SQLParameter(assessorNote);
         
         if (elementID == Util.INT_ID_EMPTY) { // Determines claim type
             sql = "SELECT fn_UpdateEvidence(?,?,?,?,?,?)";
-            super.doPreparedStatement(sql, p1, p2, p3, p4, p5, p6);
+            super.doPreparedStatement(sql, p1, p2, p3, p4, p5);
         } else {
             sql = "SELECT fn_UpdateEvidence(?,?,?,?,?,?,?)";
-            super.doPreparedStatement(sql, p1, p2, p3, p4, p5, p6, p7);
+            SQLParameter p6 = new SQLParameter(elementID);
+            super.doPreparedStatement(sql, p1, p2, p3, p4, p5, p6);
         }
     }
     
     /**
      * 
      * @param claimID
-     * @param studentID
      * @param moduleID
      * @return 
      */
-    public Evidence getByID(int claimID, String studentID, String moduleID, Integer elementID) {
+    public Evidence getByID(int claimID, String moduleID, Integer elementID) {
+        
         String sql = "SELECT * FROM fn_GetEvidence(?,?,?,?)";
-        Evidence evidence = null;
-        SQLParameter p1, p2, p3, p4;
-        p1 = new SQLParameter(claimID);
-        p2 = new SQLParameter(studentID);
-        p3 = new SQLParameter(moduleID);
-        p4 = new SQLParameter(elementID);
+        
+        SQLParameter p1 = new SQLParameter(claimID);
+        SQLParameter p2 = new SQLParameter(moduleID);
+        SQLParameter p3 = new SQLParameter(elementID);
+        
         try {
-            ResultSet rs = super.doPreparedStatement(sql, p1, p2, p3, p4);
+            ResultSet rs = super.doPreparedStatement(sql, p1, p2, p3);
             if (rs.next()) {
                 boolean approved = rs.getBoolean(Field.APPROVED.name);
                 String assessorNote = rs.getString(Field.ASSESSOR_NOTE.name);
                 String description = rs.getString(Field.DESCRIPTION.name);
-                evidence = new Evidence(claimID, studentID, moduleID, description);
+                Evidence evidence = new Evidence(claimID, moduleID, description);
                 evidence.setElementID(elementID);
                 evidence.setApproved(approved);
                 evidence.setAssessorNote(assessorNote);
+                return evidence;
             }
+            
         } catch (SQLException ex) {
             Logger.getLogger(EvidenceIO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return evidence;
+        
+        return null;
+        
     }
     
     /**
@@ -137,38 +126,38 @@ public class EvidenceIO extends RPL_IO<Evidence> {
      * @param claimedModule
      * @return 
      */
-    public ArrayList<Evidence> getList(int claimID, String studentID, String moduleID) {
-        ArrayList<Evidence> list = null;
+    public ArrayList<Evidence> getList(int claimID, String moduleID) {
         
         String sql = "SELECT * FROM fn_ListEvidence(?,?,?)";
-        SQLParameter p1, p2, p3;
-        p1 = new SQLParameter(studentID);
-        p2 = new SQLParameter(claimID);
-        p3 = new SQLParameter(moduleID);
         
-        ResultSet rs;
+        SQLParameter p1 = new SQLParameter(claimID);
+        SQLParameter p2 = new SQLParameter(moduleID);
+        
         try {
-            rs = super.doPreparedStatement(sql, p1, p2, p3);
-            list = new ArrayList<Evidence>();
-            int elementID;
-            boolean approved;
-            String description;
-            String assessorNote;
+            
+            ResultSet rs = super.doPreparedStatement(sql, p1, p2);
+            ArrayList<Evidence> list = new ArrayList<Evidence>();
+
             while (rs.next()) {
-                elementID = rs.getInt(Field.ELEMENT_ID.name); // returns 0 if elementID is null
-                approved = rs.getBoolean(Field.APPROVED.name);
-                assessorNote = rs.getString(Field.ASSESSOR_NOTE.name);
-                description = rs.getString(Field.DESCRIPTION.name);
-                Evidence evidence = new Evidence(claimID, studentID, moduleID);
+                int elementID = rs.getInt(Field.ELEMENT_ID.name); // returns 0 if elementID is null
+                boolean approved = rs.getBoolean(Field.APPROVED.name);
+                String assessorNote = rs.getString(Field.ASSESSOR_NOTE.name);
+                String description = rs.getString(Field.DESCRIPTION.name);
+                Evidence evidence = new Evidence(claimID, moduleID);
                 evidence.setApproved(approved);
                 evidence.setAssessorNote(assessorNote);
                 evidence.setDescription(description);
                 evidence.setElementID(elementID);
                 list.add(evidence);
             }
+            
+            return list;
+            
         } catch (SQLException ex) {
             Logger.getLogger(EvidenceIO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return list;
+        
+        return null;
+        
     }
 }
