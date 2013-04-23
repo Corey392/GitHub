4b1d5e3986bcb071a4ff85e2a06b97b61e4e8548
Bryce <Bryce@Bryce-PC>
2013-04-23 14:14:18 +1000

    Removed unnecessary import.

--- src/java/web/MaintainCampusServlet.java
+++ src/java/web/MaintainCampusServlet.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
