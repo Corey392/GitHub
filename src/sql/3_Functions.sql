bb6a97fd6ac0406e79164dcf11f643346d69e876
Bryce Carr <brycedcarr@gmail.com>
2013-04-25 10:52:19 +1000

    Reformatted changelog 3.51 line
    When I opened it in Notepad++ I noticed that it was way off the other
    lines, because of the way Netbeans does tab spaces (it just adds
    multiple space characters).

--- src/sql/3_Functions.sql
+++ src/sql/3_Functions.sql
@@ -10,7 +10,7 @@
  *				v3.3: Todd: Added fn_doesUserExist: Allows you to search for a user by their ID or email and will return their email address.
  *				v3.4: Todd: Updated 'fn_updatestudent' to incorporate all columns that have been added.
  *				v3.5: Todd: Added 'fn_changePassword' to allow you to change a users current password to one supplied.
- *                             v3.51: Bryce: Changed the generated-password overload of 'fn_insertUser' to use a CHARACTER for Role instead of TEXT.
+ *				v3.51: Bryce: Changed the generated-password overload of 'fn_insertUser' to use a CHARACTER for Role instead of TEXT.
  * Pre-conditions: Database must be created, tables must already exist, functions must not already exist.
  */
 
