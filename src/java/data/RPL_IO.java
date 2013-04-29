8ff4d16b3b592eed19db2c46860d78e3611b6dc1
yoshifp <todd.wiggins3@tafensw.net.au>
2013-04-29 17:37:45 +1000

    Made Database usernames static. No longer logs in a thousand times.

--- src/java/data/RPL_IO.java
+++ src/java/data/RPL_IO.java
@@ -11,52 +11,71 @@ import java.util.logging.Logger;
 
 /**
  * Defines some fields common to all of the IO classes.
+ *
  * @author Adam Shortall
  */
-public abstract class RPL_IO <T> {
+public abstract class RPL_IO<T> {
 
     private RPLConnection conn;
+    private static RPLConnection connAdmin;
+    private static RPLConnection connAssessor;
+    private static RPLConnection connStudent;
+    private static RPLConnection connTeacher;
     public final Role role;
 
     /**
      * Sets the connection type for the RPL_IO object being constructed.
+     *
      * @param role the role of the current user.
      */
     public RPL_IO(Role role) {
         this.role = role;
         switch (role) {
             case ADMIN:
-                this.conn = RPLConnection.ADMIN;
-				Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as ADMIN");
+                if (connAssessor == null) {
+                    connAssessor = RPLConnection.ADMIN;
+                    Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as ADMIN");
+                }
+                this.conn = connAssessor;
                 break;
             case CLERICAL:
-                this.conn = RPLConnection.CLERICAL;
-				Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as CLERICAL");
+                if (connAdmin == null) {
+                    connAdmin = RPLConnection.CLERICAL;
+                    Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as CLERICAL");
+                }
+                this.conn = connAdmin;
                 break;
             case STUDENT:
-                this.conn = RPLConnection.STUDENT;
-				Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as STUDENT");
+                if (connStudent == null) {
+                    connStudent = RPLConnection.STUDENT;
+                    Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as STUDENT");
+                }
+                this.conn = connStudent;
                 break;
             case TEACHER:
-                this.conn = RPLConnection.TEACHER;
-				Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as TEACHER");
+                if (connTeacher == null) {
+                    connTeacher = RPLConnection.TEACHER;
+                    Logger.getLogger(RPL_IO.class.getName()).log(Level.INFO, "Logging in as TEACHER");
+                }
+                this.conn = connTeacher;
                 break;
             default:
-                throw new IllegalArgumentException("Invalid role supplied for RPL_IO.setConn(Role), Role is: "+(role != null ? role.toString() : "null"));
+                throw new IllegalArgumentException("Invalid role supplied for RPL_IO.setConn(Role), Role is: " + (role != null ? role.toString() : "null"));
         }
     }
 
     /**
-     * Does an executeQuery on the sql String specified. Returns a
-     * ResultSet of results from the database.
+     * Does an executeQuery on the sql String specified. Returns a ResultSet of
+     * results from the database.
      *
-     * @param sql The sql statement. The values of any parameters have to be passed in as
-     * part of this String.
+     * @param sql The sql statement. The values of any parameters have to be
+     * passed in as part of this String.
      *
      * @return ResultSet from doing the query
-     * @throws SQLException the database threw an exception, check exception's SQLState for Postgre exception code.
+     * @throws SQLException the database threw an exception, check exception's
+     * SQLState for Postgre exception code.
      */
-    protected ResultSet doQuery(String sql) throws SQLException{
+    protected ResultSet doQuery(String sql) throws SQLException {
         try {
             conn.openDatabase();
             Statement statement = conn.getConnection().createStatement();
@@ -67,12 +86,15 @@ public abstract class RPL_IO <T> {
     }
 
     /**
-     * Used when needing to call a function in the database, or any statement with parameters.
+     * Used when needing to call a function in the database, or any statement
+     * with parameters.
      *
-     * @param sql The sql statement, any parameters should be represented as question marks.
-     * @param params the parameters, which must be entered in the same order that they
-     * appear in the database.
-     * @throws SQLException the database threw an exception, check exception's SQLState for Postgre exception code.
+     * @param sql The sql statement, any parameters should be represented as
+     * question marks.
+     * @param params the parameters, which must be entered in the same order
+     * that they appear in the database.
+     * @throws SQLException the database threw an exception, check exception's
+     * SQLState for Postgre exception code.
      */
     protected ResultSet doPreparedStatement(String sql, SQLParameter... params) throws SQLException {
 
@@ -105,7 +127,7 @@ public abstract class RPL_IO <T> {
                         statement.setNull(i, Types.INTEGER);
                         break;
                     default:
-                        throw new IllegalArgumentException ("SQLParameter has wrong type");
+                        throw new IllegalArgumentException("SQLParameter has wrong type");
                 }
             }
             return statement.executeQuery();
@@ -115,8 +137,9 @@ public abstract class RPL_IO <T> {
     }
 
     /**
-     * Convenience method to return a SQLParameter without having to declare
-     * a new object.
+     * Convenience method to return a SQLParameter without having to declare a
+     * new object.
+     *
      * @param value Parameter's value.
      * @return The SQLParameter.
      */
