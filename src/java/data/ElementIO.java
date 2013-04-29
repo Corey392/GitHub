82236b562a6ae86fb13118d7502ce9369e4139b3
yoshifp <todd.wiggins3@tafensw.net.au>
2013-04-29 21:25:19 +1000

    ElementIO: Stop flood of exceptions when accessing claims.
    Added check if data was returned from DB in 'getId' before trying to use the data.

--- src/java/data/ElementIO.java
+++ src/java/data/ElementIO.java
@@ -9,24 +9,24 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
- * @author James, Adam Shortall, Bryce Carr
- * @version 1.02
+ * <b>Purpose:</b>  Controller class for interaction with database's Element table.
+ * @author James, Adam Shortall, Bryce Carr, Todd Wiggins
+ * @version 1.03
  * <b>Created:</b>  Unknown<br/>
- * <b>Modified:</b> 24/04/2013<br/>
+ * <b>Modified:</b> 29/04/2013<br/>
  * <b>Change Log:</b>  08/04/2013:  Bryce Carr: Added code to incorporate moduleID DB field.<br/>
  *                  24/04/2013: Bryce Carr  Added header comments to match code conventions.<br/>
- * <b>Purpose:</b>  Controller class for interaction with database's Element table.
+ *                  29/04/2013: TW: Added check if data was returned from DB in 'getId' before trying to use the data.<br/>
  */
 public class ElementIO extends RPL_IO <Element> {
-    
+
     public enum Field {
         ELEMENT_ID("elementID"),
         MODULE_ID("moduleID"),
         DESCRIPTION("description");
-        
+
         public final String name;
-        
+
         Field(String name) {
             this.name = name;
         }
@@ -35,7 +35,7 @@ public class ElementIO extends RPL_IO <Element> {
     public ElementIO(Role role) {
         super(role);
     }
-    
+
     /**
      * Inserts or Updates an Element object depending on the function parameter.
      * @param element the element object to save
@@ -46,14 +46,14 @@ public class ElementIO extends RPL_IO <Element> {
         String description = element.getDescription();
         String sql = "SELECT fn_InsertElement(?,?)";
         SQLParameter p1 = new SQLParameter(moduleID);
-        SQLParameter p2 = new SQLParameter(description);        
+        SQLParameter p2 = new SQLParameter(description);
         super.doPreparedStatement(sql, p1, p2);
     }
 
     /**
      * Updates the description field of a specified element.
      * @param element The element object with updated description
-     * @throws SQLException 
+     * @throws SQLException
      */
     public void update(Element element) throws SQLException {
         Integer elementID = element.getElementID();
@@ -69,7 +69,7 @@ public class ElementIO extends RPL_IO <Element> {
     /**
      * Deletes an element.
      * @param element
-     * @throws SQLException 
+     * @throws SQLException
      */
     public void delete(Element element) throws SQLException {
         Integer elementID = element.getElementID();
@@ -79,12 +79,12 @@ public class ElementIO extends RPL_IO <Element> {
         SQLParameter p2 = new SQLParameter(moduleID);
         super.doPreparedStatement(sql, p1, p2);
     }
-    
+
     /**
      * Returns an element by ID.
      * @param moduleID
      * @param elementID
-     * @return 
+     * @return
      */
     public Element getByID(String moduleID, int elementID) {
         Element element = new Element();
@@ -93,11 +93,13 @@ public class ElementIO extends RPL_IO <Element> {
         SQLParameter p1, p2;
         p1 = new SQLParameter(moduleID);
         p2 = new SQLParameter(elementID);
-        try {            
+        try {
             rs = super.doPreparedStatement(sql, p1, p2);
             element = new Element(elementID, moduleID);
             rs.next();
-            element.setDescription(rs.getString(Field.DESCRIPTION.name));
+    		if (rs.getRow() > 0) {
+			   element.setDescription(rs.getString(Field.DESCRIPTION.name()));
+			}
         } catch (SQLException ex) {
             Logger.getLogger(ElementIO.class.getName()).log(Level.SEVERE, null, ex);
         }
