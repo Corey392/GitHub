aabb538c4d980af5cab4b37619be22832a6d32cc
koroshiya <korocresent@gmail.com>
2013-05-05 12:17:13 +1000

    Update RPLValidator.java
    Updated to adhere to coding standards

--- src/java/util/RPLValidator.java
+++ src/java/util/RPLValidator.java
@@ -1,17 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package util;
 
 /**
- *
  * @author rpl
  */
 public class RPLValidator {
 
     public static int fixParseIntEx(String fixStr, int replaceNum) {
-        if (fixStr != null) fixStr = fixStr.replaceAll(",", "");
+        if (fixStr != null) {
+            fixStr = fixStr.replaceAll(",", "");
+        }
         return (fixStr == null || fixStr.equals("")) ? replaceNum : Integer.parseInt(fixStr);
     }
 }
