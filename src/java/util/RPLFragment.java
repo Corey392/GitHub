61d0f6bd91a2de1eb8f162f754ac3fff7bc04103
Ryan <ryandonaldson@hotmail.com>
2013-05-17 12:35:42 +1000

    Restored RPLFragment
    Turns out its essential for making the first page work

---
+++ src/java/util/RPLFragment.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Adam Shortall
+ */
+public enum RPLFragment {
+    HEADER("/WEB-INF/jspf/header.jspf"),
+    FOOTER("/WEB-INF/jspf/footer.jspf"),
+    MAINTENANCE_OPTIONS("/WEB-INF/jspf/maintenanceOptions.jspf"),
+    JAVASCRIPT("/scripts/RPLScripts.js");
+
+    public final String relativeAddress;
+    public final String absoluteAddress;
+
+    RPLFragment(String relativeAddress) {
+        this.relativeAddress = relativeAddress;
+        this.absoluteAddress = RPLPage.ROOT + relativeAddress;
+    }
+
+    @Override
+    public String toString() {
+        return this.absoluteAddress;
+    }
+}
