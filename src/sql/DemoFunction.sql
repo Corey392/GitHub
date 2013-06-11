d2be355fcfa141a923bae9c1eb876bcbf45a7521
t355273971 <t355273971@OITS-112009.uncle.newcastle.edu.au>
2013-06-10 21:35:08 -0300

    Demo Function of the insert student for the presentation

---
+++ src/sql/DemoFunction.sql
@@ -0,0 +1,16 @@
+--
+-- Name: fn_insertstudent(text, text, text, text, text, text, text, text, text, integer, text, text, boolean); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION fn_insertstudent("userID" text, "email" text, "firstName" text, "lastName" text, "otherName" text, "addressLine1" text, "addressLine2" text, "town" text, "state" text, "postCode" integer, "phoneNumber" text, "studentID" text, "staff" boolean) RETURNS text
+    LANGUAGE plpgsql
+    AS $_$
+    DECLARE pw TEXT;
+
+    BEGIN
+		SELECT INTO pw fn_InsertUser($1, 'S', $2, $3, $4);
+		INSERT INTO "Student"("userID", "otherName", "addressLine1", "addressLine2", "town", "state", "postCode", "phoneNumber", "studentID", "staff")
+			VALUES($1, $5, $6, $7, $8, $9, $10, $11, $12, $13);
+		RETURN pw;
+    END;
+$_$;
